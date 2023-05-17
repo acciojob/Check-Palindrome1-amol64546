bd05a48e07e06f353ad538a2db7a6182b709ccc9
Amol Nakhate <106758417+amol64546@users.noreply.github.com>
2023-05-17 13:30:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // complete the given function
 
 function palindrome(str){
-
+	let str1 = str.toLowerCase();
+	let i=0, j=str1.length-1;
+	while(i<j){
+		if(str1[i]!==str1[j]) return false;
+		i++;
+		j--;
+	}
+	return true;
 }
 module.exports = palindrome
